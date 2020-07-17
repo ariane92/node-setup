be37040c321f8d319c6cb6b599146c5d7bed8913
Ariane Mateus <ariane.ccomp@gmail.com>
2020-07-17 14:04:58 -0300

    feat: Added Eslint and Jest

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message : ' Hello dev'});
-});  
+app.get('/', (request, response) => response.json({ message: ' Hello dev' }));
 
 app.listen(3333);
